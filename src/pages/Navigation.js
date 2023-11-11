92ebabe0d5f67ff3611a6fcc264628cba8012c6c
Kaloyan <kaloyanbobev@gmail.com>
2023-11-11 20:41:43 +0000

    Create Navigation.js

---
+++ src/pages/Navigation.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
+
+export default function Navigation() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Login</Link>
+          </li>
+          <li>
+            <Link to="payslip">Payslip</Link>
+          </li>
+          <li>
+            <Link to="calendar">Calendar</Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+    </>
+  );
+}
