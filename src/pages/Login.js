3898d860e6cf91b169fafc418071fb76deee4bf2
Kaloyan <kaloyanbobev@gmail.com>
2023-11-14 12:10:49 +0000

    add the login form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,44 @@
 import React from "react";
+
 import { Link } from "react-router-dom";
 
 export default function Login() {
   return (
     <div>
-      <form>
+      <div className="Auth-form-container">
+        <form className="Auth-form">
+          <div className="Auth-form-content">
+            <h3 className="Auth-form-title">Sign In</h3>
+            <div className="form-group mt-3">
+              <label>Email address</label>
+              <input
+                type="email"
+                className="form-control mt-1"
+                placeholder="Enter email"
+              />
+            </div>
+            <div className="form-group mt-3">
+              <label>Password</label>
+              <input
+                type="password"
+                className="form-control mt-1"
+                placeholder="Enter password"
+              />
+            </div>
+            <div className="d-grid gap-2 mt-3">
+              <button type="submit" className="btn btn-primary">
+                <Link to="payslip" className="btn btn-lg">
+                  Submit
+                </Link>
+              </button>
+            </div>
+            <p className="forgot-password text-right mt-2">
+              Forgot <a href="./pages/PageNotFound.js">password?</a>
+            </p>
+          </div>
+        </form>
+      </div>
+      {/* <form>
         <label>Username</label>
         <input type="text" placeholder="your username" required></input>
         <label>Password</label>
@@ -12,7 +46,7 @@ export default function Login() {
         <Link to="payslip" className="btn btn-lg">
           Submit
         </Link>
-      </form>
+      </form> */}
     </div>
   );
 }
