bce7be70ae48f4a4e57a6720334c4b039c649d1e
Kaloyan <kaloyanbobev@gmail.com>
2023-11-14 12:11:24 +0000

    update

--- src/pages/Navigation.js
+++ src/pages/Navigation.js
@@ -6,7 +6,9 @@ export default function Navigation() {
   return (
     <>
       <Nav defaultActiveKey="/" className="flex-row">
-        <Nav.Link href="/">Login</Nav.Link>
+        <Nav.Link exact path href="/">
+          Login
+        </Nav.Link>
         <Nav.Link href="payslip">Payslip</Nav.Link>
         <Nav.Link href="download">Download Your Payslips</Nav.Link>
         <Nav.Link href="calendar">Calendar</Nav.Link>
