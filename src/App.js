eade37e43653a34057fa293d1c636456c40157a2
Kaloyan <kaloyanbobev@gmail.com>
2023-11-14 12:09:04 +0000

    Add new page 404
    add path for each of the routes who are not specified to to 404 page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import DownloadPayslips from "./pages/DownloadPayslips";
 import Navigation from "./pages/Navigation";
 import Details from "./pages/Details";
 import Security from "./pages/Security";
+import NotFound from "./pages/PageNotFound";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Route path="download" element={<DownloadPayslips />} />
         <Route path="details" element={<Details />} />
         <Route path="security" element={<Security />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
