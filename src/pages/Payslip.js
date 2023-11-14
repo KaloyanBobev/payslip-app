12d71b986f50b4cb18b72819e7271e88768e6918
Kaloyan <kaloyanbobev@gmail.com>
2023-11-14 12:12:32 +0000

    change te color to blue

--- src/pages/Payslip.js
+++ src/pages/Payslip.js
@@ -5,7 +5,7 @@ export default function Payslip() {
     <div className="container">
       <h1>Payslip for John Smith for Monthly period 7 2023/2024</h1>
       <div className="container">
-        <div className="row bg-info">
+        <div className="row bg-primary">
           <div className="col-md-2">Employee ID</div>
           <div className="col-md-2">Employee Names</div>
           <div className="col-md-2">Employer</div>
@@ -14,14 +14,14 @@ export default function Payslip() {
           <div className="col-md-2">Tax period</div>
         </div>
         <div className="row">
-          <div className="col-md-2 border border-info">125125</div>
-          <div className="col-md-2 border border-info">John Smith</div>
-          <div className="col-md-2 border border-info">UWL</div>
-          <div className="col-md-2 border border-info">120/123dsss</div>
-          <div className="col-md-2 border border-info">27/10/2023</div>
-          <div className="col-md-2 border border-info">7</div>
+          <div className="col-md-2 border border-primary">125125</div>
+          <div className="col-md-2 border border-primary">John Smith</div>
+          <div className="col-md-2 border border-primary">UWL</div>
+          <div className="col-md-2 border border-primary">120/123dsss</div>
+          <div className="col-md-2 border border-primary">27/10/2023</div>
+          <div className="col-md-2 border border-primary">7</div>
         </div>
-        <div className="row clearfix bg-info">
+        <div className="row clearfix bg-primary">
           <div className="col-md-2 ">Payments</div>
           <div className="col-md-2 float-end">Value</div>
           <div className="col-md-2">Deduction</div>
@@ -29,7 +29,7 @@ export default function Payslip() {
           <div className="col-md-2">Year to date</div>
           <div className="col-md-2 float-end">Value</div>
         </div>
-        <div className="row clearfix border border-info">
+        <div className="row clearfix border border-primary">
           <div className="col-md-2 ">
             <div>Salary</div>
             <div>Total</div>
@@ -66,28 +66,31 @@ export default function Payslip() {
         <div className="row">
           <span className="col-md-4 border">
             <div className="row">
-              <div className="col-md-6 bg-info"> Tax Code</div>
-              <div className="col-md-6 border border-info"> 1257L</div>
+              <div className="col-md-6 bg-primary"> Tax Code</div>
+              <div className="col-md-6 border border-primary"> 1257L</div>
             </div>
             <div className="row">
-              <div className="col-md-6 bg-info">NI Number</div>
-              <div className="col-md-6 border border-info"> SE012688A</div>
+              <div className="col-md-6 bg-primary">NI Number</div>
+              <div className="col-md-6 border border-primary"> SE012688A</div>
             </div>
             <div className="row">
-              <div className="col-md-6 bg-info">NI Category</div>
-              <div className="col-md-6 border border-info"> A</div>
+              <div className="col-md-6 bg-primary">NI Category</div>
+              <div className="col-md-6 border border-primary"> A</div>
             </div>
             <div className="row">
-              <div className="col-md-6 bg-info">HMRC pay ID</div>
-              <div className="col-md-6 border border-info"> 1933</div>
+              <div className="col-md-6 bg-primary">HMRC pay ID</div>
+              <div className="col-md-6 border border-primary"> 1933</div>
             </div>
             <div className="row">
-              <div className="col-md-6 bg-info">Departament</div>
-              <div className="col-md-6 border border-info"> IT Departament</div>
+              <div className="col-md-6 bg-primary">Departament</div>
+              <div className="col-md-6 border border-primary">
+                {" "}
+                IT Departament
+              </div>
             </div>
           </span>
-          <span className="col-md-4 border border-info">
-            <div className="row  bg-info">Other payments</div>
+          <span className="col-md-4 border border-primary">
+            <div className="row  bg-primary">Other payments</div>
             <div className="row b">
               <div className="col-md-6">Employer's NI</div>
               <div className="col-md-6">£178.12</div>
@@ -98,7 +101,7 @@ export default function Payslip() {
             </div>
           </span>
         </div>
-        <div className="row border border-info">
+        <div className="row border border-primary">
           <div className="col-md-6">Net Pay</div>
           <div className="col-md-6 ">
             <b>£2012.33</b>
