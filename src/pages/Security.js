b0c47703606e522816b5b4659175ddb5492b4129
Kaloyan <kaloyanbobev@gmail.com>
2023-11-16 12:06:54 +0000

    create a security question form

--- src/pages/Security.js
+++ src/pages/Security.js
@@ -1,10 +1,21 @@
 import React from "react";
 
 export default function Security() {
-  return <div>
-    <form>
-      <label></label>
-      <input type=""></input>
-    </form>
-  </div>;
+  return (
+    <div>
+      <form className="center-block">
+        <div className="row ">
+          <label className="col-md-2">Change Password</label>
+          <input className="col-md-2 border border-primary" type="text"></input>
+        </div>
+        <div className="row">
+          <label className="col-md-2">Change Security Question</label>
+          <input className="col-md-2 border border-primary" type="text"></input>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Change
+        </button>
+      </form>
+    </div>
+  );
 }
