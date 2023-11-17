c7361b02c3f8301fcc685d313da1bb23e85b780a
Kaloyan <kaloyanbobev@gmail.com>
2023-11-17 11:37:22 +0000

    Update PageNotFound.js
    adding a classes and paragraph and image

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,11 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import img01 from "../img/004.jpg";
 
 export default function PageNotFound() {
   return (
-    <div>
-      <h1>Page Not Found</h1>
-      <Link to="/">Go Home</Link>
+    <div className="container page-not row">
+      <div className="col-md-3">
+        <h1>Page Not Found</h1>
+        <p>
+          Sorry, tha page you are looking for could not be found or has been
+          removed
+        </p>
+        <Link to="/">Return to login page </Link>
+      </div>
+      <div className="col-md-4">
+        <img src={img01} alt="404 not found amazing " />
+      </div>
     </div>
   );
 }
