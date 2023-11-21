1205131d424c6b4d661fa582ddc105312431204f
Kaloyan <kaloyanbobev@gmail.com>
2023-11-21 21:51:27 +0000

    Update Security.js

--- src/pages/Security.js
+++ src/pages/Security.js
@@ -1,21 +1,30 @@
 import React from "react";
-
+// import Navigation from "./Navigation";
 export default function Security() {
   return (
-    <div>
-      <form className="center-block">
-        <div className="row ">
-          <label className="col-md-2">Change Password</label>
-          <input className="col-md-2 border border-primary" type="text"></input>
-        </div>
-        <div className="row">
-          <label className="col-md-2">Change Security Question</label>
-          <input className="col-md-2 border border-primary" type="text"></input>
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Change
-        </button>
-      </form>
-    </div>
+    <>
+      {/* <Navigation /> */}
+      <div>
+        <form className="center-block">
+          <div className="row ">
+            <label className="col-md-2">Change Password</label>
+            <input
+              className="col-md-2 border border-primary"
+              type="text"
+            ></input>
+          </div>
+          <div className="row">
+            <label className="col-md-2">Change Security Question</label>
+            <input
+              className="col-md-2 border border-primary"
+              type="text"
+            ></input>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Change
+          </button>
+        </form>
+      </div>
+    </>
   );
 }
