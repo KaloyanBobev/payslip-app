295945d2bc8c064cf28156edf3ac7b91b9871f5f
Kaloyan <kaloyanbobev@gmail.com>
2023-11-21 21:50:38 +0000

    Update Calendar.js

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -2,31 +2,35 @@ import React from "react";
 import { useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
-
+// import Navigation from "./Navigation";
 export default function CalendarElement() {
   const [date, setDate] = useState(new Date());
   return (
-    <div className="container">
-      <h1>Viewing Holiday Calendar for John Smith</h1>
-      <Calendar
-        className="center-block"
-        onChange={setDate}
-        value={date}
-        selectRange={true}
-        defaultView="decade"
-      />
-      {date.length > 0 ? (
-        <p className="text-center">
-          <span className="bold">Start:</span> {date[0].toDateString()}
-          &nbsp;|&nbsp;
-          <span className="bold">End:</span> {date[1].toDateString()}
-        </p>
-      ) : (
-        <p className="text-center">
-          <span className="bold">Default selected date:</span>{" "}
-          {date.toDateString()}
-        </p>
-      )}
-    </div>
+    <>
+{/*
+      <Navigation /> */}
+      <div className="container">
+        <h1>Viewing Holiday Calendar for John Smith</h1>
+        <Calendar
+          className="center-block"
+          onChange={setDate}
+          value={date}
+          selectRange={true}
+          defaultView="decade"
+        />
+        {date.length > 0 ? (
+          <p className="text-center">
+            <span className="bold">Start:</span> {date[0].toDateString()}
+            &nbsp;|&nbsp;
+            <span className="bold">End:</span> {date[1].toDateString()}
+          </p>
+        ) : (
+          <p className="text-center">
+            <span className="bold">Default selected date:</span>{" "}
+            {date.toDateString()}
+          </p>
+        )}
+      </div>
+    </>
   );
 }
