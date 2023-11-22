f86216dfb5e21d0e1ccbbcb97478c42eeafbe47b
Kaloyan <kaloyanbobev@gmail.com>
2023-11-22 21:05:17 +0000

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { MdVisibility, MdVisibilityOff } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
