1d3a88efd7d1c39514964a0b8e94f0d5f6011b1f
Kaloyan <kaloyanbobev@gmail.com>
2023-12-02 22:23:51 +0000

    Create ForgotPassword.js
    adding a new page for forgoten password with

---
+++ src/pages/ForgotPassword.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { useState } from "react";
+export default function ForgotPassword() {
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div>
+      <div className="Auth-form-container">
+        <form className="Auth-form" onSubmit={handleSubmit}>
+          {<p className="d-flex justify-content-center text-danger"></p>}
+          <div className="Auth-form-content">
+            <h3 className="Auth-form-title">Forgoten Password</h3>
+            <div className="form-group mt-3">
+              <label htmlFor="email">Email address</label>
+              <input
+                type="email"
+                className="form-control mt-1"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+
+            <div className="d-grid gap-2 mt-3">
+              {/*submit button with onClick method checking is the entered email and password are match with those in database if is matching rwill redirect to payslip page*/}
+              <button type="submit" className="btn btn-primary">
+                Send Conformation
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
