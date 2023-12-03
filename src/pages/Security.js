958ce7b2a1c5c62e410a2854cb57ea3b78c5dfa0
Kaloyan <kaloyanbobev@gmail.com>
2023-12-03 13:30:29 +0000

    Update Security.js
    adding a new message and some margin

--- src/pages/Security.js
+++ src/pages/Security.js
@@ -1,28 +1,46 @@
 // import the base code from React library
-import React from "react";
-// import Navigation from "./Navigation";
+import React, { useState } from "react";
+
+import Navigation from "./Navigation";
+
 export default function Security() {
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setMessage("Details Changed");
+  };
+
   //JSX for changing a password and security question on the user profile
   return (
     <>
-      {/* <Navigation /> */}
-      <div>
-        <form className="center-block">
-          <div className="row ">
+      <Navigation />
+      <div className="container">
+        <h1>Change your security details</h1>
+
+        <form className="center-block" onSubmit={handleSubmit}>
+          {message && (
+            <p className="d-flex justify-content-left text-danger">{message}</p>
+          )}
+          <div className="row mb-2">
             <label className="col-md-2">Change Password</label>
             <input
               className="col-md-2 border border-primary"
               type="text"
             ></input>
           </div>
-          <div className="row">
+          <div className="row mb-2">
             <label className="col-md-2">Change Security Question</label>
             <input
               className="col-md-2 border border-primary"
               type="text"
             ></input>
           </div>
-          <button type="submit" className="btn btn-primary">
+          <button
+            type="submit"
+            onSubmit={handleSubmit}
+            className="btn btn-primary"
+          >
             Change
           </button>
         </form>
